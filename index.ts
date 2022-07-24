6a9d4158d83f51bef4f9c42c89e7c5baf00c7a21
Chris Menz <chrismenz@Chriss-MacBook-Pro.local>
2022-07-24 11:34:02 -0400

    cd

--- index.ts
+++
@@ -1,9 +0,0 @@
-const express = require("express")
-const app = express()
-const port = 8080
-
-app.get("/", (req, res, next) => {
-    res.send("hello world!")
-})
-
-app.listen(port)
---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import express, { Express, Request, Response } from "express"
+
+const app: Express = express()
+const port = 3003
+
+app.get("/", (req: Request, res: Response) => {
+    res.send("ss dev server")
+})
+
+app.listen(port)
