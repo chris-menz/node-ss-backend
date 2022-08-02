f3ecc3ec4b76350cc6bad77915db9be28278ba0d
Chris Menz <chrismenz@Chriss-MacBook-Pro.local>
2022-08-02 19:51:50 -0400

    allow origin

--- src/index.ts
+++ src/index.ts
@@ -40,9 +40,19 @@ app.use(function(req, res, next) {
     next();
   });
 
+  app.use("/graphql", function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    if (req.method === 'OPTIONS') {
+      res.sendStatus(200);
+    } else {
+      next();
+    }
+  });
 
 
-app.use("/graphql", cors() ,graphqlHTTP({
+
+app.use("/graphql", graphqlHTTP({
     schema,
     rootValue: root,
     graphiql: true,
