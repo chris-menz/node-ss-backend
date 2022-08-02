23092c193ccc963d92f70e7ad3e7304708580873
Chris Menz <chrismenz@Chriss-MacBook-Pro.local>
2022-08-02 19:38:48 -0400

    allow origin

--- src/index.ts
+++ src/index.ts
@@ -16,6 +16,16 @@ app.use(cors({
         origin: ["*"],
         methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
         credentials: true,
+        allowedHeaders: [
+            'X-App-Version',
+            'Accept',
+            'Accept-Version',
+            'Content-Type',
+            'Api-Version',
+            'Origin',
+            'X-Requested-With',
+            'Authorization',
+        ]
     }
 ))
 app.use(express.json())
