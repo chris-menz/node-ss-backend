9c5bf7239f6b054d85b73aeeab6638883350445e
Chris Menz <chrismenz@Chriss-MacBook-Pro.local>
2022-08-02 19:45:43 -0400

    allow origin

--- src/index.ts
+++ src/index.ts
@@ -33,13 +33,14 @@ app.use(cookieParser())
 
 const root = resolvers
 
+app.use(cors())
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
 
-app.use(cors())
+
 
 app.use("/graphql", graphqlHTTP({
     schema,
